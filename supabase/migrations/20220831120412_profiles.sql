9d228d84375802641a509022e7c6e79aa59d81a6
Ak4zh <agwl.akash@gmail.com>
2022-08-31 17:40:15 +0530

    added user profiles table

---
+++ supabase/migrations/20220831120412_profiles.sql
@@ -0,0 +1,28 @@
+create table "public"."profiles" (
+    "id" uuid not null,
+    "updated_at" timestamp with time zone not null default now(),
+    "full_name" text,
+    "username" text
+);
+
+
+alter table "public"."profiles" enable row level security;
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+CREATE UNIQUE INDEX profiles_username_key ON public.profiles USING btree (username);
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+alter table "public"."profiles" add constraint "profiles_username_key" UNIQUE using index "profiles_username_key";
+
+
+create policy "profiles.allow_select_own_profile" on profiles
+  for select using (auth.uid() = id);
+
+create policy "profiles.allow_update_own_profile" on profiles
+  for update using (auth.uid() = id);
