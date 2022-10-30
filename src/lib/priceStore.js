f9d1981d71a3fce316e169be30979aeaba244e9c
ak4zh <agwl.akash@gmail.com>
2022-10-31 02:42:32 +0530

    store cleanup

--- src/lib/priceStore.js
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-const priceStore = writable({});
-
-export default priceStore;
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,3 +2,4 @@ import { writable } from "svelte/store";
 import type { Writable } from "svelte/store";
 
 export let loading: Writable<boolean> = writable(false)
+export let priceStore = writable({});
